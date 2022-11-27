9663bdf0871d796c393c4d708566a4b1b47acb78
leig <leig@usc.edu>
2022-11-26 22:05:50 -0800

    update

---
+++ build_x86/CMakeFiles/blas_mec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blas_mec.dir/blas_mec.cpp.o"
+  "blas_mec"
+  "blas_mec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blas_mec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
